e2a96b8b4db7325aeaca248a4daa1ac8334a6405
StackDoubleFlow <ojaslandge@gmail.com>
2022-10-09 14:17:40 -0500

    Check headset battery on seperate thread

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,47 @@
 use anyhow::{Context, Result};
+use libg933::battery::{BatteryStatus, ChargingStatus};
 use serde::{Deserialize, Serialize};
 use std::io::{BufRead, BufReader};
 use std::process::{Command, Stdio};
+use std::sync::Mutex;
+use std::thread;
+use std::time::Duration;
+
+static BATTERY_STATUS: Mutex<Option<BatteryStatus>> = Mutex::new(None);
 
 #[derive(Deserialize, Serialize, Debug)]
-struct Block {
-    name: String,
-    markup: String,
-    full_text: String,
+struct Block<'a> {
+    name: &'a str,
+    markup: &'a str,
+    full_text: &'a str,
 }
 
-fn headset_volume() -> Result<String> {
-    let output = Command::new("headsetcontrol").args(["-c", "-b"]).output()?;
-    Ok(String::from_utf8(output.stdout)?)
+fn update_battery_status() {
+    let mut devices = libg933::find_devices().unwrap();
+    if devices.is_empty() {
+        let mut lock = BATTERY_STATUS.lock().unwrap();
+        *lock = None;
+        return;
+    }
+    let device = devices.values_mut().next().unwrap();
+
+    if let Ok(battery_status) = device.get_battery_status() {
+        let mut lock = BATTERY_STATUS.lock().unwrap();
+        *lock = Some(battery_status);
+    }
+}
+
+fn headset_battery() -> Result<String> {
+    let battery_status = BATTERY_STATUS.lock().unwrap();
+    Ok(if let Some(battery_status) = &*battery_status {
+        let mut status = format!("{:.01}%", battery_status.charge);
+        if matches!(battery_status.charging_status, ChargingStatus::Charging(_)) {
+            status.push_str(" [\u{f583}]")
+        }
+        status
+    } else {
+        "Disconnected".to_string()
+    })
 }
 
 fn main() -> Result<()> {
@@ -24,28 +53,27 @@ fn main() -> Result<()> {
     println!("{}", header);
     println!("[");
 
+    thread::spawn(|| loop {
+        update_battery_status();
+        thread::sleep(Duration::from_secs(5))
+    });
+
     for blocks in status_lines.skip(1) {
         let blocks = blocks?;
         let blocks = blocks.trim_start_matches(',');
         let mut blocks: Vec<Block> = serde_json::from_str(blocks)?;
 
-        let headset_volume = headset_volume()?;
-        let headset_volume = if headset_volume == "-1" {
-            "Charging".to_string()
-        } else if headset_volume.is_empty() {
-            "Disconnected".to_string()
-        } else {
-            format!("{}%", headset_volume)
-        };
+        let headset_battery = headset_battery()?;
+        let full_text = format!(
+            "<span background='#007e91'> \u{F7CD} {} </span>",
+            headset_battery
+        );
         blocks.insert(
             0,
             Block {
-                name: "headset_volume".to_string(),
-                markup: "pango".to_string(),
-                full_text: format!(
-                    "<span background='#007e91'> \u{F7CD} {} </span>",
-                    headset_volume
-                ),
+                name: "headset_battery",
+                markup: "pango",
+                full_text: &full_text,
             },
         );
 
